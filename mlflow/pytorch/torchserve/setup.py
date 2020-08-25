4a8fbc03bce27e5a134dde8cd2baea748b6803cb
Shrinath Suresh <shrinath@ideas2it.com>
2020-08-26 00:22:03 +0530

    Setting plugin name to mlflow-torchserve

--- mlflow/pytorch/torchserve/setup.py
+++ mlflow/pytorch/torchserve/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name="torchservedeployment",
+    name="mlflow-torchserve",
     version="0.0.1",
     description="Torch Serve Mlflow Deployment",
     packages=find_packages(),
