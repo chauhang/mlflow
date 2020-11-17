51af47eac8efc25cae1560dac49f0d175b799f62
Shrinath Suresh <shrinath@ideas2it.com>
2020-11-17 15:15:00 +0530

    Resetting empty line
    Signed-off-by: Shrinath Suresh <shrinath@ideas2it.com>

--- mlflow/pytorch/_pytorch_autolog.py
+++ mlflow/pytorch/_pytorch_autolog.py
@@ -137,6 +137,7 @@ def _autolog(log_every_n_epoch=1):
             :param trainer: pytorch lightning trainer instance
             :param pl_module: pytorch lightning base module
             """
+
             mlflow.pytorch.log_model(pytorch_model=trainer.model, artifact_path="model")
 
             if self.early_stopping and trainer.checkpoint_callback.best_model_path:
